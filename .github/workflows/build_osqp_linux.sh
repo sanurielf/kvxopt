63a55029e8cd7f9ef32bf621b9eaa932f71ffa79
Uriel Sandoval <sanurielf@gmail.com>
2021-03-26 18:12:45 -0600

    Add prebuild command to compile and install OSQP inside manylinux docker

---
+++ .github/workflows/build_osqp_linux.sh
@@ -0,0 +1,21 @@
+# @Author: Uriel Sandoval
+# @Date:   2021-03-26 18:10:10
+# @Last Modified by:   Uriel Sandoval
+# @Last Modified time: 2021-03-26 18:11:22
+
+
+# Get OSQP
+git clone --recursive https://github.com/oxfordcontrol/osqp.git
+cd osqp
+git checkout 0.6.0
+
+
+# Get OSQP
+cd osqp
+mkdir build
+cd build
+cmake ..
+sudo cmake --build . --target install
+sudo ldconfig -v
+
+
